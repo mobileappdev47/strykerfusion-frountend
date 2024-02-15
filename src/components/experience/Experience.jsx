bfde774f41664f48c2cada562933e500a0f42c39
mobileappdev47 <mobileappdev47@gmail.com>
2024-02-15 10:32:53 +0530

    changes in expireance component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
-import styles from './experience.module.css'
+import React from 'react';
+import style from './experience.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const Experience = () => {
     return (
-        <div className={`${styles.container}`}>
+        <div className={`${style.maindiv}`}>
             <div className="row">
-                <div className="col-3">
-                    <h1>Experience the Difference</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, repudiandae delectus ea commodi debitis quaerat cumque assumenda fugit possimus temporibus.</p>
+                <div className="col-4">
+                    <h1 className={style.headingfont}>Experience the Difference</h1>
+                    <p className={style.content}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, repudiandae delectus ea commodi debitis quaerat cumque assumenda fugit possimus temporibus.</p>
+                    <button className={`btn ${style.learnmorebtn}`}>Learn More</button>
                 </div>
-                <div className="col-3">
-                    
-                </div>
-                <div className="col-3">
-                    col3
-                </div>
-                <div className="col-3">
-                    col3
+                <div className="col-8">
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={30}
+                        navigation={{ nextEl: '.swiper-button-next' }} // Enable only the right arrow
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>Slide 1</SwiperSlide>
+                        <SwiperSlide>Slide 2</SwiperSlide>
+                        <SwiperSlide>Slide 3</SwiperSlide>
+                        <SwiperSlide>Slide 4</SwiperSlide>
+                        <SwiperSlide>Slide 5</SwiperSlide>
+                        <SwiperSlide>Slide 6</SwiperSlide>
+                        <SwiperSlide>Slide 7</SwiperSlide>
+                        <SwiperSlide>Slide 8</SwiperSlide>
+                        <SwiperSlide>Slide 9</SwiperSlide>
+                    </Swiper>
+                    <div className="swiper-button-next"></div> {/* Right arrow */}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Experience
+export default Experience;
