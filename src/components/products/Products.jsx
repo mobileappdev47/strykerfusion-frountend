13872ad0f8d28830d72ec7988ce3796b9317f4a1
mobileappdev47 <mobileappdev47@gmail.com>
2024-02-29 17:33:23 +0530

    changes in product components

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import style from './products.module.css';
-import { motion, useInView } from 'framer-motion';
+import { AnimatePresence, motion, useAnimation, useInView } from 'framer-motion';
 import { base_url } from '../config/Base_url';
 import axios from 'axios';
-import classnames from "classnames";
 
 const Products = () => {
   const [productMain, setProductMain] = useState([]);
@@ -60,38 +59,14 @@ const Products = () => {
       console.error('Error fetching home data:', error);
     }
   };
-
+  console.log(isVisible)
   useEffect(() => {
     fetchData();
   }, []);
 
-  const [visibleImagesMap, setVisibleImagesMap] = useState(
-    products.reduce((map, image) => { // Corrected the method name to 'reduce'
-      map[image] = false;
-      return map;
-    }, {})
-  );
+  const controls = useAnimation();
 
-  useLayoutEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = document.documentElement.scrollTop;
-      const viewportHeight = window.innerHeight;
-
-      const newVisibleImagesMap = products?.reduce((map, image) => { // Corrected the method name to 'reduce'
-        map[image] = scrollTop >= image * viewportHeight;
-        return map;
-      }, {});
-
-      setVisibleImagesMap(newVisibleImagesMap);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    handleScroll();
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-
+  console.log(isVisible)
   return (
     <>
       <div className={style.maindiv}>
@@ -99,44 +74,55 @@ const Products = () => {
           <h1 className={style.headingfont}>{productMain?.productTitle}</h1>
           <p className={style.content}>{productMain?.productDescription}</p>
         </div>
-        {products?.map((item, index) => (
-          <div className={`${style.imgsection} py-4 px-sm-3  `} key={index}>
-            <div ref={inViewRef} className='h-100'>
-              <div className='position-relative'>
-                <div className={style.imagegradient}></div>
-                <img
-                  className={`h-100 w-100`}
-                  src={`${base_url}/${item?.productImage}`} alt='product' />
+        <div className={style.parentscrolldiv} ref={ref} >
+          {products?.map((item, index) => (
+            <AnimatePresence>
+              {isVisible && (
+                <div
+                  className={`${style.imgsection}  py-4 px-sm-3`} key={index} id={`product_${item._id}`}
+                >
+                  <motion.div initial={{ opacity: 0, y: 100 }}
+                    animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : 10 }}
+                    transition={{ duration: 0.5 }} className={` position-relative h-100`}>
+                    <div className={style.imagegradient}></div>
+                    <img
+                      className={`h-100 w-100`}
+                      src={`${base_url}/${item?.productImage}`} alt='product' />
 
-                <div className={style.contentbox}>
-                  <h1 className={style.imgheadingfont}>
-                    {item?.productTitle}
-                  </h1>
-                  <h1 className={style.imgcontent}>
-                    View Project
-                  </h1>
+                    <div className={style.contentbox}>
+                      <h1 className={style.imgheadingfont}>
+                        {item?.productTitle}
+                      </h1>
+                      <h1 className={style.imgcontent}>
+                        View Project
+                      </h1>
+                    </div>
+                  </motion.div>
                 </div>
-              </div>
-            </div>
-          </div>
-        ))}
-        <div className={`row ${style.imgsection}`}>
-          {products?.slice(0, 4)?.map((item, index) => (
-            <div ref={ref} className="col-6 px-1 pb-3 px-sm-3 h-50 d-flex justify-content-center align-items-center">
-              <motion.div
-                className={`${style.imageContainer} h-100`}
-              >
-                <div className={style.imagegradient}></div>
-                <img src={`${base_url}/${item?.productImage}`} className={`${style.productimage} img-fluid`} alt='product' />
-                <div className={style.contentbox}>
-                  <h1 className={style.headingfontimg}>{item?.productTitle}</h1>
-                  <h1 className={style.allimgcontent}>
-                    View Project
-                  </h1>
-                </div>
-              </motion.div>
-            </div>
+              )}
+            </AnimatePresence>
           ))}
+          <AnimatePresence>
+            {isVisible && (
+              <div className={`row ${style.imgsection}`}>
+                {products?.slice(0, 4)?.map((item, index) => (
+                  <div ref={ref} className="col-6 px-1 pb-3 px-sm-3 h-50 d-flex justify-content-center align-items-center">
+                    <motion.div
+                      className={`${style.imageContainer} h-100`}
+                    >
+                      <div className={style.imagegradient}></div>
+                      <img src={`${base_url}/${item?.productImage}`} className={`${style.productimage} img-fluid`} alt='product' />
+                      <div className={style.contentbox}>
+                        <h1 className={style.headingfontimg}>{item?.productTitle}</h1>
+                        <h1 className={style.allimgcontent}>
+                          View Project
+                        </h1>
+                      </div>
+                    </motion.div>
+                  </div>
+                ))}
+              </div>)}
+          </AnimatePresence>
         </div>
       </div>
     </>
