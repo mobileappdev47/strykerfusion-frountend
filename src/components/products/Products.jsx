b6058eaf8c5d19f38e441f18f822dc6c8c4c0e56
mobileappdev <mobileappdev47@gmail.com>
2024-03-20 17:32:57 +0530

    product animate

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import style from './products.module.css';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import axios from 'axios';
+import SingleProduct from '../singleproduct/SingleProduct';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -25,12 +26,6 @@ const Products = () => {
     offset: ['start start', 'end end']
   });
 
-  const container1 = useRef(null);
-  const { scrollY } = useScroll({
-    target: container1,
-    offset: ['start end', 'start start']
-  });
-
   const scales = [
     useTransform(scrollYProgress, [0, 1], [2.8, 1]),
     useTransform(scrollYProgress, [0, 1], [3.6, 1]),
@@ -38,8 +33,6 @@ const Products = () => {
     useTransform(scrollYProgress, [0, 1], [3.6, 1]),
     useTransform(scrollYProgress, [0, 1], [3.6, 1])
   ];
-
-  // scales[2] = useTransform(scrollXProgress, [0, 1], [3.6, 1]);
 
   const tops = [
     useTransform(scrollYProgress, [0, 1], ['30%', '0%']),
@@ -61,44 +54,13 @@ const Products = () => {
     useTransform(scrollYProgress, [0, 0.5, 1], [0, 0.5, 1])
   ];
 
-  // Rearrange the products array to place the last image at the beginning
   const rearrangedProducts = products.length > 0 ? [products[products.length - 1], ...products.slice(0, products.length - 1)] : [];
   const firstMapProducts = products.slice(0, products.length - 1);
-  const imageVariants = {
-    hidden: { opacity: 0, scale: 0.5 },
-    visible: { opacity: 1, scale: 1 }
-  };
-  
+
   return (
     <>
       <div>
-        <div>
-          {firstMapProducts.map((product, index) => (
-            <div key={index}>
-              <div className={`${style.maindiv}`}>
-              <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={imageVariants}
-                  transition={{ duration: 0.5 }}
-                  className={`${style.imgsection} mx-sm-3`}>
-                  <div className=''>
-                    <div className={style.imagegradient}></div>
-                    <img
-                      className={style.singleproimg}
-                      src={`${process.env.REACT_APP_BASE_URL}/${product?.productImage}`}
-                      alt='product'
-                    />
-                  </div>
-                  <div className={style.contentbox}>
-                    <h1 className={style.imgheadingfont}>{product?.productTitle}</h1>
-                    <h1 className={style.imgcontent}>View Project</h1>
-                  </div>
-                </motion.div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <SingleProduct />
         <div ref={container} className={style.container}>
           <div className={`row ${style.sticky}`}>
             {rearrangedProducts?.map(({ productImage, productTitle }, index) => (
---
+++ src/components/singleproduct/SingleProduct.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Box from "@mui/material/Box";
+import axios from "axios";
+import style from './singleproduct.module.css'
+
+gsap.registerPlugin(ScrollTrigger);
+
+const SingleProduct = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `${process.env.REACT_APP_BASE_URL}/product`
+                );
+                setProducts(response?.data?.data);
+            } catch (error) {
+                console.error("Error fetching product data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if (products.length > 0) {
+            let ctx = gsap.context(() => {
+                gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0.5 });
+
+                const animation = gsap.to(".photo:not(:first-child)", {
+                    opacity: 1,
+                    scale: 1,
+                    duration: 1,
+                    stagger: 1
+                });
+
+                ScrollTrigger.create({
+                    trigger: ".gallery",
+                    start: "top top",
+                    end: "bottom bottom",
+                    pin: ".rightblock",
+                    animation: animation,
+                    scrub: true
+                });
+            });
+
+            return () => ctx.revert();
+        }
+    }, [products]);
+
+    const firstMapProducts = products.slice(0, products.length - 1);
+
+    return (
+        <React.Fragment>
+            <Box className="gallery" sx={{ display: "flex" }}>
+                <Box
+                    className="left"
+                    sx={{
+                        width: "0%",
+                        marginLeft: "auto",
+                        "& .details": {
+                            height: "100vh",
+                            display: "flex",
+                            flexDirection: "column",
+                            justifyContent: "center",
+                            width: "40vw",
+                            marginLeft: "auto",
+                            color: "#000",
+                            fontSize: "3rem",
+                            fontWeight: 900
+                        }
+                    }}
+                >
+                    <Box className="details"></Box>
+                    <Box className="details"></Box>
+                    <Box className="details"></Box>
+                </Box>
+                <Box
+                    className="rightblock"
+                    sx={{
+                        width: "100%",
+                        height: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center"
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: "69.8vw",
+                            height: "40vh",
+                            position: "relative",
+                            top: "30%",
+                            left: "15%",
+                            "& .photo": {
+                                position: "absolute",
+                                width: "100%",
+                                height: "100%",
+                                "& img": {
+                                    height: "100%",
+                                    width: "100%"
+                                }
+                            }
+                        }}
+                    >
+                        {firstMapProducts.map((product, index) => (
+                            <Box className="photo">
+                                <div key={index}>
+                                    <div className={`${style.maindiv}`}>
+                                        <div
+                                            className={`${style.imgsection} mx-sm-3`}>
+                                            <div className={style.imagegradient}></div>
+                                            <img
+                                                className={style.singleproimg}
+                                                src={`${process.env.REACT_APP_BASE_URL}/${product?.productImage}`}
+                                                alt='product'
+                                            />
+                                            <div className={style.contentbox}>
+                                                <h1 className={style.imgheadingfont}>{product?.productTitle}</h1>
+                                                <h1 className={style.imgcontent}>View Project</h1>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+            </Box>
+        </React.Fragment>
+    );
+};
+
+export default SingleProduct;
