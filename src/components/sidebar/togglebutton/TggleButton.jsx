526de16fcc1a8322803caa49c44bfeb34568f851
mobileappdev47 <mobileappdev47@gmail.com>
2024-04-13 10:12:44 +0530

    toggle button changes

--- src/components/sidebar/togglebutton/TggleButton.jsx
+++ src/components/sidebar/togglebutton/TggleButton.jsx
@@ -9,6 +9,7 @@ const ToggleButton = ({ setOpen }) => {
           strokeWidth="3"
           stroke=" #265EE1"
           strokeLinecap="round"
+          initial={{ d: "M 2 2.5 L 20 2.5" }}
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" },
@@ -28,6 +29,7 @@ const ToggleButton = ({ setOpen }) => {
           strokeWidth="3"
           stroke="#265EE1"
           strokeLinecap="round"
+          initial={{ d: "M 2 16.346 L 20 16.346" }}
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" },
